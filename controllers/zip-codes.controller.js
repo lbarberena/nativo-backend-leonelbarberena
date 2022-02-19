1f03c1d4669d62b9c50e2f2d2089bad7f338fe58
lbarberena <octaviobarberena@hotmail.com>
2022-02-19 12:02:09 -0600

    Delete unused controller

--- controllers/zip-codes.controller.js
+++ controllers/zip-codes.controller.js
@@ -2,37 +2,6 @@
 const axios = require('axios')
 
 module.exports = {
-    GET: async function ( req, res, next ) {
-        try {
-            axios
-            .get('https://www.wikitable2json.com/api/Postal_codes_in_Nicaragua?lang=en&format=keyValue&cleanRef=true')
-            .then( resp => {
-                const codesArray = resp.data[0];
-                codesArray.forEach( element => {
-                    element.zipCode = element["Postal code"];
-                    delete element["Postal code"];
-                });
-                return res.status( 200 ).send({
-                    success: true,
-                    msg: 'Zip codes',
-                    data: resp.data[0],
-                });
-            })
-            .catch( error => {
-                return res.status( 200 ).send({
-                    success: false,
-                    msg: 'Error ocurred',
-                    data: error,
-                });
-            });
-        } catch( error ) {
-            return res.status( 500 ).send({
-                success: false,
-                msg: 'ERROR',
-                data: {}
-            });
-        }
-    },
     GetZipCodes: async function ( req, res, next ) {
         try {
             axios
--- routes/zip-codes.route.js
+++ routes/zip-codes.route.js
@@ -1,8 +1,7 @@
 'use strict';
-let { GET, GetZipCodes, GetByZipCode } = require('../controllers/zip-codes.controller');
+let { GetZipCodes, GetByZipCode } = require('../controllers/zip-codes.controller');
 
 module.exports = (api) => {
-    api.get('/zipcodes/all', GET);
     api.get('/zipcodes', GetZipCodes);
     api.get('/zipcodes/:zipCode', GetByZipCode);
 };
